da1393264dc43aa7434e4912ac5e72f1b0d70395
David Brockman Smoliansky <david@springworks.se>
2019-07-01 17:54:44 +0200

    fix git auth issue with a temp .netrc file

--- scripts/actions/local-yarn-command/entrypoint.sh
+++ scripts/actions/local-yarn-command/entrypoint.sh
@@ -11,6 +11,11 @@ if [ -n "$GIT_USER_NAME" ]; then
   git config --global user.name "$GIT_USER_NAME"
 fi
 
+if [[ -n "$GH_TOKEN" ]] && [[ -n "$GH_USERNAME" ]]; then
+  echo "machine github.com\nlogin $GH_USERNAME\npassword $GH_TOKEN\n\nmachine api.github.com\nlogin $GH_USERNAME\npassword $GH_TOKEN" > ~/.netrc
+  chmod 0600 ~/.netrc
+fi
+
 if [ -n "$NPM_AUTH_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
